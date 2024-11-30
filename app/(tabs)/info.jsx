46465411862c0b414e854f69e9ae2411eed0578f
unknown <the.yunglegend10@gmail.com>
2024-11-30 19:07:31 -0300

    info screen

--- app/(tabs)/info.jsx
+++ app/(tabs)/info.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Linking, Button} from 'react-native'
+import { Text, View, TouchableOpacity} from 'react-native'
 import React, {useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { openURL, canOpenURL } from 'expo-linking'
@@ -9,14 +9,36 @@ const info = () => {
   canOpenURL("tel:+5581987019406").then(canOpen => setCanOpenTelephone(canOpen));
 
   return (
-    <SafeAreaView className="justify-center items-center flex-1">
-        {/* <Text className="justify-center items-center"> Número Coordenação </Text> */}
-        <Button className= "bg-white"
-        onPress={() => openURL("tel:+5581987019406")} title="Número Coordenação"/>
-        <Button className= "bg-white"
-        onPress={() => openURL("mailto:admtest@gmail.com")} title="Email Coordenação"/>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="my-4 p-4">
+        <Text className="font-inter-bold text-blue-600 text-4xl">Informações</Text>
+      </View>
+        <View className="p-4 gap-4">
+        <Text className="font-inter-regular text-2xl text-slate-500 pb-4">Mediotec Recife</Text>
+          <TouchableOpacity className="rounded-md bg-blue-600 flex-start mx-16 p-4" onPress={() => openURL("whatsapp://send?text=Olá&phone=+5581987019406")}>
+            <Text className="font-inter-regular text- text-white text-center">Número Coordenação</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="rounded-md bg-blue-600 flex-start mx-16 p-4" onPress={() => openURL("mailto:admtest@gmail.com")}>
+            <Text className="font-inter-regular text- text-white text-center">Email Coordenação</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="rounded-md bg-blue-600 flex-start mx-16 p-4" onPress={() => openURL("https:https//www.youtube.com/watch?v=Iig0kFj9Glg")}>
+            <Text className="font-inter-regular text- text-white text-center">Financeiro</Text>
+          </TouchableOpacity>
+        </View>
+          <View className="p-4 gap-4">
+          <Text className="font-inter-regular text-2xl text-slate-500 pb-4">Mediotec Paulista</Text>
+            <TouchableOpacity className="rounded-md bg-blue-600 flex-start mx-16 p-4" onPress={() => openURL("whatsapp://send?text=Olá&phone=+5581987019406")}>
+              <Text className="font-inter-regular text- text-white text-center">Número Coordenação</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="rounded-md bg-blue-600 flex-start mx-16 p-4" onPress={() => openURL("mailto:admtest@gmail.com")}>
+              <Text className="font-inter-regular text- text-white text-center">Email Coordenação</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="rounded-md bg-blue-600 flex-start mx-16 p-4" onPress={() => openURL("https://www.youtube.com/watch?v=Iig0kFj9Glg")}>
+              <Text className="font-inter-regular text- text-white text-center">Financeiro</Text>
+            </TouchableOpacity>
+          </View>
     </SafeAreaView>
-);
+  );
 }
 
 export default info
